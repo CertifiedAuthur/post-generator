f4eb0e7b62581fd84b98c1e3931d9f7c2fd4dbb1
Chibuzor Okafor <134807240+CertifiedAuthur@users.noreply.github.com>
2024-11-09 23:27:35 +0100

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -1,11 +1,13 @@
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-import os
 
 
-load_dotenv()
+# Define Groq API key and model name
+GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
+MODEL_NAME = "llama-3.2-90b-text-preview"
 
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name = "llama-3.2-90b-text-preview")
+# Initialize LLM model using ChatGroq
+llm = ChatGroq(groq_api_key=GROQ_API_KEY, model_name=MODEL_NAME)
 
 if __name__ == "__main__":
     response = llm.invoke("What are the two main ingradients in bitter leaf soup")
